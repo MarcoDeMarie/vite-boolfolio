7e3f98f93c3d83b197fa1f45d95420a14c6f35fc
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-06-28 16:13:54 +0200

    creata pagina app

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+
+<script>
+export default {
+    name:'home',
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>elenco posts</h1>
+    </div>
+</template>
+
+<style>
+
+</style>
