ccbf840f93f37b13b90a878029d73f3a7859bec2
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-07-01 16:00:09 +0200

    router e buttons

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::namespace('Api')
         ->prefix('posts')
         ->group(function(){
             Route::get('/',[PostController::class, 'index']);
+            Route::get('/categories',[PostController::class, 'getCategories']);
         });
